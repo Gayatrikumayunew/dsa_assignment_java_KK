1b9b21571f10582d836d65f9494887ca9eec864b
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2026-03-03 13:58:30 +0530

    Add files via upload

---
+++ _08_SI_canculation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+
+public class _08_SI_canculation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int principal=sc.nextInt();
+        int rate=sc.nextInt();
+        int time=sc.nextInt();
+        int SimpleInterest=(principal*rate*time)/100;
+        System.out.println(SimpleInterest);
+
+    }
+}
