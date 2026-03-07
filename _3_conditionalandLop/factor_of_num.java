a2ae7444867ea944cf81bf86225b374b75edfa22
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2026-03-07 23:53:23 +0530

    Add files via upload

---
+++ _3_conditionalandLop/factor_of_num.java
@@ -0,0 +1,13 @@
+package _3_conditionalandLop;
+//Input a number and print all the factors of that number (use loops).
+public class factor_of_num {
+    public static void main(String[] args) {
+        int num=22;
+        for (int i = 1; i <=12 ; i++) {
+            if (num%i==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
