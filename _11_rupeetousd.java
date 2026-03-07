ad2ffeb9631814ed42d08a742f0b34876ae92714
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2026-03-07 23:24:32 +0530

    Delete _11_rupeetousd.java

--- _11_rupeetousd.java
+++
@@ -1,9 +0,0 @@
-public class _11_rupeetousd {
-    public static void main(String[] args) {
-        float rs=725;
-        float usd;
-        float exchangerate=91;
-        usd=rs/exchangerate;
-        System.out.println(usd);
-    }
-}
